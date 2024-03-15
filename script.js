ae4674277283ed2e36b52830b0ac9e0278593cac
shubham-tam <tamrakarshubham23@gmail.com>
2024-03-16 00:08:10 +0545

    feat: refacot done

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-var props = document.getElementById("props");
-var result = document.getElementById("result");
+import {
+  handlePropsGeneration,
+  isLastStringNotAComma,
+  replaceLineBreakWithComma,
+} from "./utils";
+
+const props = document.getElementById("props");
+const result = document.getElementById("result");
+
 props.addEventListener("input", function (event) {
-    var textAreaValue = props.value;
-    var getPropsResult = function (value) {
-        var string = value;
-        if (string.charAt(string.length - 1) !== ",") {
-            var formattedString = string + ",";
-            return handlePropsGeneration(formattedString);
-        }
-        else {
-            return handlePropsGeneration(string);
-        }
-    };
-    var handlePropsGeneration = function (string) {
-        var stringSplitByComma = string
-            .split(",")
-            .map(function (str) { return str.trim(); });
-        return stringSplitByComma
-            .filter(function (str) { return str.length !== 0; })
-            .map(function (str) { return "".concat(trimWhiteSpace(str), "={").concat(trimWhiteSpace(str), "}"); })
-            .join("\n");
-    };
-    var trimWhiteSpace = function (string) {
-        return string.replace(/\s/g, "");
-    };
-    result.innerHTML = getPropsResult(textAreaValue.replace(/\n/g, ", "));
+  const textAreaValue = props.value;
+
+  const getPropsResult = (value) => {
+    const string = value;
+
+    if (isLastStringNotAComma(string)) {
+      const formattedString = string + ",";
+
+      return handlePropsGeneration(formattedString);
+    } else {
+      return handlePropsGeneration(string);
+    }
+  };
+
+  result.innerHTML = getPropsResult(
+    replaceLineBreakWithComma(textAreaValue)(/\n/g, ", ")
+  );
 });
---
+++ utils/index.js
@@ -0,0 +1,20 @@
+export const isLastStringNotAComma = (string) => {
+  return string.charAt(string.length - 1) !== ",";
+};
+
+export const trimWhiteSpace = (string) => {
+  return string.replace(/\s/g, "");
+};
+
+export const handlePropsGeneration = (string) => {
+  const stringSplitByComma = string.split(",").map((str) => str.trim());
+
+  return stringSplitByComma
+    .filter((str) => str.length !== 0)
+    .map((str) => `${trimWhiteSpace(str)}={${trimWhiteSpace(str)}}`)
+    .join("\n");
+};
+
+export const replaceLineBreakWithComma = (string) => {
+  return string.replace(/\n/g, ", ");
+};
